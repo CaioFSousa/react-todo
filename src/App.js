4d7dee5cdc57d4233e5ea0a49272a4540844a4b5
Felipe Sousa <103897841+CaioFSousa@users.noreply.github.com>
2024-01-03 19:17:30 +0000

    feat: Added React Icons
    Added 'react-icons' package to package.json and imported icons in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+    setTodos((oldState) => [...oldState, todo]);
+    console.log(todo);
+
+    setTitle("");
+    setTime("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+
+      <div className="form-todo">
+        <h2>Insert a new todo</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">What will you do?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Todo title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duration:</label>
+            <input
+              type="time"
+              name="time"
+              placeholder="Estimated time in hours"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+          <input type="submit" value="Create Task" />
+        </form>
+      </div>
+
+      <div className="list-todo">
+        <h2>Todo List</h2>
+        {todos.length === 0 && <p>You dont have any todo</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3>{todo.title}</h3>
+            <p>Duration: {todo.time}</p>
+            <div className="actions">
+              <span>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
